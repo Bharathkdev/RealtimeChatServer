2f842b3ee68896d7eca76debdf7b8d96030d606e
Bharathkdev <bharathaskkarur46@gmail.com>
2022-12-25 23:30:06 +0530

    Removed unwanted files

--- test.js
+++
@@ -1,9 +0,0 @@
-const WebSocket = require('ws');
-
-const ws = new WebSocket('ws://localhost:3000');
-
-ws.on('open', () => {
-  ws.on('message', (message) => {
-    console.log(message);
-  })
-});
--- test1.js
+++
@@ -1,8 +0,0 @@
-const WebSocket = require('ws');
-
-const ws = new WebSocket('wss://c208-121-200-48-218.in.ngrok.io');
-
-ws.on('open', () => {
-  ws.send(JSON.stringify({id: new Date().getTime(), type: 'order', name: '27', contact: "7777777787", itemsPlaced: "Car", delivery: "1st Jan 2023", time: new Date().getTime()}));
-  ws.close();
-});
